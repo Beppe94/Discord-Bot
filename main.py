2bcef5cbbc6d99b855bfa293f4712774e69df058
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-09-26 21:59:48 +0200

    bot now reads gif attachments and added bot spam prevention

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+import PIL
 
 command_prefix = 'w.'
 load_dotenv('.env')
@@ -13,9 +14,16 @@ client = discord.Client(intents=discord.Intents.all())
 async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'{command_prefix}'))
 
+
 @client.event
 async def on_message(message):
+    if message.author.bot:
+        return
     if message.author != client.user and message.content.startswith(command_prefix):
         await message.channel.send(f'Hi {message.author.mention}')
+    elif 'gif' in message.content:
+        await message.channel.send('This is a gif')
+        
+
 
 client.run(TOKEN)
