8ea08d1cfc3840a7563eb9b461a7c5d0d2276a50
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-10-02 22:15:03 +0200

    added function to convert kelvin to celsius

--- weather.py
+++ weather.py
@@ -14,6 +14,16 @@ def weather(message):
         url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={API}'
         data = requests.get(url).content
         prettyfied = json.loads(data)
-        return prettyfied
+        data_list = []
+        data_list.append(prettyfied['weather'])
+        data_list.append(temp_c(prettyfied['main']))
+        return data_list
         
     
+def temp_c(data_dict):
+    for k,v in data_dict.items():
+        if k == 'temp' or k == 'feels_like' or k == 'temp_min' or k == 'temp_max':
+            data_dict[k] = v - 273.15
+        else:
+            data_dict[k] = v
+    return data_dict
