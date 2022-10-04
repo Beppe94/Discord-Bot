946d0432966d5b75a520d72600151cf1c3ce759f
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-10-04 23:13:19 +0200

    added location check and slice to max 2 decimal digits the temperature

--- weather.py
+++ weather.py
@@ -1,4 +1,5 @@
 import json
+from xml.dom import NotFoundErr
 import requests
 from dotenv import load_dotenv
 import os 
@@ -9,21 +10,27 @@ command_prefix = 'w.'
 
 def weather(message):
     location = message.content.replace(command_prefix, '')
-
+    
     if len(location) >= 1:
         url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&appid={API}'
         data = requests.get(url).content
         prettyfied = json.loads(data)
-        data_list = []
-        data_list.append(prettyfied['weather'])
-        data_list.append(temp_c(prettyfied['main']))
-        return data_list
-        
+        print(prettyfied)
+        if prettyfied['cod'] == '404':
+            return 'Location not found'
+        else:
+            data_list = []
+            data_list.append(prettyfied['weather'])
+            data_list.append(temp_c(prettyfied['main']))
+            return data_list
     
+
+#convert kelvin temp into celisus
+#need to reduce decimal digits
 def temp_c(data_dict):
     for k,v in data_dict.items():
         if k == 'temp' or k == 'feels_like' or k == 'temp_min' or k == 'temp_max':
-            data_dict[k] = v - 273.15
+            data_dict[k] = "{:.2f}".format(v-273.15)
         else:
             data_dict[k] = v
     return data_dict
